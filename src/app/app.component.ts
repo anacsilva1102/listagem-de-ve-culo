82bfd0209e513d26829fcba9bb69d1d87e71913b
Ana Clara <ana.c.silva1102@aluno.senai.br>
2026-07-31 15:30:47 -0300

    subir arquivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'listagem-veiculos';
+
+  modelo: string = '';
+  valor: number | null = null;
+
+  lista: any[] = [];
+
+  cadastrar() {
+
+    if (this.modelo.trim() === '' || this.valor === null || this.valor <= 0) {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    this.lista.push({
+      modelo: this.modelo.toUpperCase(),
+      valor: this.valor
+    });
+
+    this.modelo = '';
+    this.valor = null;
+
+  }
+
+  remover(indice: number) {
+
+    this.lista.splice(indice, 1);
+
+  }
+
 }
